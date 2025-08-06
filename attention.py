8f51dc13d9038a7c75c6033e1a68c1ed63cfdf87
Aravind-808 <halfwind2005@gmail.com>
2025-08-06 09:52:56 +0530

    encoding, embedding, attn added

---
+++ attention.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import math
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d, heads):
+        super(MultiHeadAttention, self).__init__()
+        assert d%heads == 0
+
+        self.d = d
+        self.heads = heads
+        self.d_k = d//heads
+
+        self.W_q = nn.Linear(d, d) # query
+        self.W_k = nn.Linear(d, d) # key
+        self.W_v = nn.Linear(d, d) # value
+        self.W_o = nn.Linear(d, d) # output projection 
+    
+    def scaled_dotproduct_attention(self, Q, K, V, mask = None):
+        attention_scores = torch.matmul(Q, K.transpose(-2, -1))/math.sqrt(self.d_k)
+        attention_probabilities = torch.softmax(attention_scores, dim = -1)
+        output = torch.matmul(attention_probabilities, V)
+
+        return output
+
+    def split_heads(self, x):
+        batch_size, seq_length, d = x.size()
+        return x.view(batch_size, seq_length, self.heads, self.d_k).transpose(1, 2)
+    
+    def combine_heads(self, x):
+        batch_size, num, seq_length,  d_k = x.size()
+        return x.transpose(1, 2).contiguous().view(batch_size, seq_length, self.d)
+
+    def forward(self, Q, K, V, mask = None):
+        Q = self.split_heads(self.W_q(Q))
+        K = self.split_heads(self.W_k(K))
+        V = self.split_heads(self.W_v(V))
+
+        attention_output = self.scaled_dotproduct_attention(Q, K, V, mask)
+        output = self.W_o(self.combine_heads(attention_output))
+        return output
+    
---
+++ encoding_embedding.py
@@ -0,0 +1,43 @@
+import torch.nn as nn
+import torch, math
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_seq_len):
+        super(PositionalEncoding, self).__init__()
+
+        pe = torch.zeros(max_seq_len, d_model)
+        pos = torch.arange(0, max_seq_len, dtype=torch.float).unsqueeze(1)
+
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000)/d_model))
+
+        pe[:, 0::2] = torch.sin(pos*div_term)
+        pe[:, 1::2] = torch.cos(pos*div_term) 
+
+        self.register_buffer('pe', pe.unsqueeze(0))
+
+    
+    def forward(self, x):
+
+        return x + self.pe[:, :x.size(1)]
+
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, d_model, image_size, patch_size, n_channels):
+        super(PatchEmbedding, self).__init__()
+
+        self.d_model = d_model
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.n_channels = n_channels
+        
+        # B: Batch Size
+        # C: Image Channels
+        # H: Image Height
+        # W: Image Width
+        # P_col: Patch Column
+        # P_row: Patch Row
+        x = self.linear_projection = nn.Conv2d(n_channels, d_model, kernel_size=patch_size, stride=patch_size) # returns B, d_model, p_col, p_row
+        x = x.flatten(2) # returns B, d_model, P (P is p_col*p_row)
+        x = x.transpose(1, 2) # returns, B, P, d_model (accepted by transfprmer)
+
+        return x
